caa811927adda07100d9c074f2872fcdd032f915
yscof <dexter1541@naver.com>
2026-04-24 19:38:29 +0900

    p.25

---
+++ chatper1/3.js
@@ -0,0 +1,26 @@
+console.log(1 + 2);
+console.log(1 * 2);
+console.log(1 - 2);
+console.log(1 / 2);
+console.log(1 % 2);
+
+console.log(1 / 0);
+
+const nan1 = 1 * "hello";
+const nan2 = 1 - "hello";
+const nan3 = 1 / "hello";
+
+console.log(nan1);
+console.log(nan2);
+console.log(nan3);
+
+let name = '이정환';
+let welcomeText = '님 반가워요!';
+let resultWelcomeText = name + welcomeText;
+
+console.log(resultWelcomeText);
+
+let guestName = "이정환";
+let greetingText = `welcome ${guestName}!`;
+
+console.log(greetingText);
