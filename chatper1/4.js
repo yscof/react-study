73789e944a6a0d9ad7b82c4ff1bd3bd30c0996e6
yscof <dexter1541@naver.com>
2026-04-24 21:05:34 +0900

    p.35

--- chatper1/4.js
+++ chatper1/4.js
@@ -13,16 +13,45 @@
 // console.log(numA);
 // console.log(numB);
 
-let strA = "10";
-let strB = "10개";
+// let strA = "10";
+// let strB = "10개";
 
-let numA = parseInt(strA, 10);
-let numB = parseInt(strB, 10);
+// let numA = parseInt(strA, 10);
+// let numB = parseInt(strB, 10);
 
-console.log(numA);
-console.log(numB);
+// console.log(numA);
+// console.log(numB);
 
-let str = "파이팅 2023";
-let num = parseInt(str, 10);
+// let str = "파이팅 2023";
+// let num = parseInt(str, 10);
 
-console.log(num);
+// console.log(num);
+
+// let num = 2022;
+// let str = String(num);
+
+// console.log(str);
+
+// let varA;
+// let varB = null;
+// let varC = true;
+
+// let strA = String(varA);
+// let strB = String(varB);
+// let strC = String(varC);
+
+// console.log(strA);
+// console.log(strB);
+// console.log(strC);
+
+let varA = "하이";
+let varB = 0;
+let varC = "";
+
+let boolA = Boolean(varA);
+let boolB = Boolean(varB);
+let boolC = Boolean(varC);
+
+console.log(boolA);
+console.log(boolB);
+console.log(boolC);
---
+++ chatper1/5.js
@@ -0,0 +1,74 @@
+// let numA = 1;
+// let numB;
+// let numC;
+
+// numB = numC = numA;
+// console.log(numA, numB, numC);
+
+// let numberA = 1;
+// let numberB = 2;
+
+// console.log(numberA + numberB);
+// console.log(numberA - numberB);
+// console.log(numberA * numberB);
+// console.log(numberA / numberB);
+// console.log(numberA % numberB);
+
+// let numberA = 1 + 2 * 10;
+
+// console.log(numberA);
+
+// let numberA = (1 + 2) * 10;
+
+// console.log(numberA);
+
+// let number = 10;
+// number = number + 10;
+
+// console.log(number);
+
+// let number = 10;
+
+// number += 10;
+// console.log(number);
+
+// number -= 10;
+// console.log(number);
+
+// number *= 10;
+// console.log(number);
+
+// number /= 10;
+// console.log(number);
+
+// number %= 10;
+// console.log(number);
+
+// let a = 1;
+// a++;
+// console.log(a);
+
+// let b = 1;
+// b--;
+// console.log(b);
+
+// let a = 1;
+// console.log(a++);
+// console.log(a);
+
+// let b = 1;
+// console.log(b--);
+// console.log(b);
+
+// let a = 1;
+// console.log(++a);
+// console.log(a);
+
+// let b = 1;
+// console.log(--b);
+// console.log(b);
+
+let boolA = true;
+let boolB = false;
+
+console.log(boolA || boolB);
