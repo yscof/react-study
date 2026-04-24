5ad13aca10ecd128abecd85980d22983ba9c70f3
yscof <dexter1541@naver.com>
2026-04-24 20:12:18 +0900

    p.29

--- chatper1/3.js
+++ chatper1/3.js
@@ -1,26 +1,44 @@
-console.log(1 + 2);
-console.log(1 * 2);
-console.log(1 - 2);
-console.log(1 / 2);
-console.log(1 % 2);
+// console.log(1 + 2);
+// console.log(1 * 2);
+// console.log(1 - 2);
+// console.log(1 / 2);
+// console.log(1 % 2);
 
-console.log(1 / 0);
+// console.log(1 / 0);
 
-const nan1 = 1 * "hello";
-const nan2 = 1 - "hello";
-const nan3 = 1 / "hello";
+// const nan1 = 1 * "hello";
+// const nan2 = 1 - "hello";
+// const nan3 = 1 / "hello";
 
-console.log(nan1);
-console.log(nan2);
-console.log(nan3);
+// console.log(nan1);
+// console.log(nan2);
+// console.log(nan3);
 
-let name = '이정환';
-let welcomeText = '님 반가워요!';
-let resultWelcomeText = name + welcomeText;
+// let name = '이정환';
+// let welcomeText = '님 반가워요!';
+// let resultWelcomeText = name + welcomeText;
 
-console.log(resultWelcomeText);
+// console.log(resultWelcomeText);
 
-let guestName = "이정환";
-let greetingText = `welcome ${guestName}!`;
+// let guestName = "이정환";
+// let greetingText = `welcome ${guestName}!`;
 
-console.log(greetingText);
+// console.log(greetingText);
+
+let name = "이정환";
+let location = "역곡";
+let introduce = `${name}은 ${location}에 살고 있습니다.`;
+
+console.log(introduce);
+
+let isSwitchOn = false;
+
+console.log(isSwitchOn);
+
+let emptyVar = null;
+
+console.log(emptyVar);
+
+let realEmptyVar;
+
+console.log(realEmptyVar);
---
+++ chatper1/4.js
@@ -0,0 +1,28 @@
+// let number = 10;
+// let string = "20";
+
+// const result = number + string;
+// console.log(result);
+
+// let strA = "10";
+// let strB = "10개";
+
+// let numA = Number(strA);
+// let numB = Number(strB);
+
+// console.log(numA);
+// console.log(numB);
+
+let strA = "10";
+let strB = "10개";
+
+let numA = parseInt(strA, 10);
+let numB = parseInt(strB, 10);
+
+console.log(numA);
+console.log(numB);
+
+let str = "파이팅 2023";
+let num = parseInt(str, 10);
+
+console.log(num);
