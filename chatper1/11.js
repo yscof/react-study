3cb9cc1d2a757e62df4fb53ede474a16d53d95b6
yscof <dexter1541@naver.com>
2026-04-28 14:23:22 +0900

    p.75

---
+++ chatper1/11.js
@@ -0,0 +1,12 @@
+// let food = ["짜장면", "피자", "치킨"];
+
+// console.log(food[0]);
+// console.log(food[1]);
+// console.log(food[2]);
+
+let food = ["짜장면", "피자", "치킨"];
+
+food[2] = "파스타";
+food[3] = "레몬";
+
+console.log(food);
