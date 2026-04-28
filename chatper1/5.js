2fc79ba09577c689408094d326d2fa3c76b7078d
yscof <dexter1541@naver.com>
2026-04-28 11:45:58 +0900

    p.38

--- chatper1/5.js
+++ chatper1/5.js
@@ -68,7 +68,62 @@
 // console.log(--b);
 // console.log(b);
 
-let boolA = true;
-let boolB = false;
+// let boolA = true;
+// let boolB = false;
 
-console.log(boolA || boolB);
+// console.log(boolA || boolB);
+
+// let boolA = true;
+// let boolB = false;
+
+// console.log(boolA && boolB);
+
+// let boolA = true;
+// let boolB = false;
+
+// console.log(!boolA);
+// console.log(!boolB);
+
+// let numberA = 2;
+// let numberB = 2;
+// let numberC = '2';
+
+// console.log(numberA === numberB);
+// console.log(numberB === numberC);
+// console.log(numberB == numberC);
+
+// let numberA = 2;
+// let numberB = 2;
+// let numberC = "2";
+
+// console.log(numberA !== numberB);
+// console.log(numberB !== numberC);
+// console.log(numberB != numberC);
+
+// let numberA = 1;
+// let numberB = 2;
+// let numberC = 3;
+
+// console.log(numberA < numberB);
+// console.log(numberA > numberB);
+// console.log(numberB < numberC);
+// console.log(numberB > numberC);
+
+// let numberA = 1;
+// let numberB = 2;
+// let numberC = 2;
+
+// console.log(numberA <= numberB);
+// console.log(numberA >= numberB);
+// console.log(numberB <= numberC);
+// console.log(numberB >= numberC);
+
+// let varA = 10;
+// let varB = 20;
+// let varC;
+
+// console.log(varA ?? varB);
+// console.log(varC ?? varB);
+
+let name;
+let nickname = "winterlood";
