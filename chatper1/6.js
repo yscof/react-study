f1efdc7df050496163b357e28d7198054b52b8bb
yscof <dexter1541@naver.com>
2026-04-28 12:15:03 +0900

    p.47

---
+++ chatper1/6.js
@@ -0,0 +1,120 @@
+// let num = 11;
+
+// if (num >= 10) {
+//     console.log("num은 10 이상입니다.");
+// }
+
+// let num = 11;
+
+// if (num >= 10) {
+//     console.log("조건 일치!");
+//     console.log("num은 10 이상입니다.");
+// }
+
+// let num = 5;
+
+// if (num >= 10) {
+//     console.log("num은 10 이상입니다.");
+// } else if (num >= 5) {
+//     console.log("num은 5 이상입니다.");
+// } else {
+//     console.log("num은 5 미만입니다.");
+// }
+
+// let country = "ko";
+
+// if (country === "ko") {
+//     console.log("한국");
+// } else if (country === "us") {
+//     console.log("미국");
+// } else if (country === "dk") {
+//     console.log("덴마크");
+// } else {
+//     console.log("미분류");
+// }
+
+// let fruit = "apple";
+
+// switch (fruit) {
+//     case "apple": {
+//         console.log("사과");
+//         break;
+//     }
+//     case "banana": {
+//         console.log("바나나");
+//         break;
+//     }
+//     default: {
+//         console.log("우리가 찾는 과일이 아님");
+//     }
+// }
+
+// let fruit = "apple";
+
+// switch (fruit) {
+//     case "apple": {
+//         console.log("사과");
+//     }
+//     case "banana": {
+//         console.log("바나나");
+//     }
+//     default: {
+//         console.log("우리가 찾는 과일이 아님");
+//     }
+// }
+
+// let country = "ko";
+
+// if (country === "ko") {
+//     console.log("한국");
+// } else if (country === "us") {
+//     console.log("미국");
+// } else if (country === "dk") {
+//     console.log("덴마크");
+// } else if (country === "do") {
+//     console.log("도미니카공화국");
+// } else if (country === "mx") {
+//     console.log("멕시코");
+// } else if (country === "ch") {
+//     console.log("스위스");
+// } else if (country === "es") {
+//     console.log("스페인");
+// } else {
+//     console.log("미분류");
+// }
+
+let country = "ko";
+
+switch (country) {
+    case "ko": {
+        console.log("한국");
+        break;
+    }
+    case "us": {
+        console.log("미국");
+        break;
+    }
+    case "dk": {
+        console.log("덴마크");
+        break;
+    }
+    case "do": {
+        console.log("도미니카공화국");
+        break;
+    }
+    case "mx": {
+        console.log("멕시코");
+        break;
+    }
+    case "ch": {
+        console.log("스위스");
+        break;
+    }
+    case "es": {
+        console.log("스페인");
+        break;
+    }
+    default: {
+        console.log("미분류");
+    }
+}
