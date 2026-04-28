620dec807c27f7507e14813305cb79547dc6ed11
yscof <dexter1541@naver.com>
2026-04-28 13:58:50 +0900

    p.68

---
+++ chatper1/9.js
@@ -0,0 +1,81 @@
+// let a = 1;
+
+// function foo() {
+//     console.log(a);
+// }
+
+// function bar() {
+//     console.log(a);
+// }
+
+// foo();
+// bar();
+// console.log(a);
+
+// function foo() {
+//     let a = 1;
+// }
+
+// console.log(a);
+
+// if (true) {
+//     let a = 1;
+// }
+
+// console.log(a);
+
+// for (let i = 0; i < 10; i++) {
+//     let a = 1;
+// }
+
+// console.log(a);
+// console.log(i);
+
+// function foo() {
+//     console.log("foo");
+// }
+
+// function bar() {
+//     foo();
+//     console.log("bar");
+// }
+
+// bar();
+
+// function foo() {
+//     console.log("foo");
+
+//     function bar() {
+//         console.log("bar");
+//     }
+// }
+
+// bar();
+
+// if (true) {
+//     function foo() {
+//         console.log("bar");
+//     }
+// }
+
+// foo();
+
+// for (let i = 1; i < 10; i++) {
+//     function bar() {
+//         console.log("bar");
+//     }
+// }
+
+// bar();
+
+// if (true) {
+//     var a = 1;
+// }
+
+// console.log(a);
+
+// function foo() {
+//     var a = 1;
+// }
+
+// console.log(a);
