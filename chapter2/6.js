03e7305d49ffc90cd36348a05ddd5ec1d68e8d7f
yscof <dexter1541@naver.com>
2026-04-28 19:21:07 +0900

    p.100

---
+++ chapter2/6.js
@@ -0,0 +1,44 @@
+// let arrA = [1, 2 ,3];
+// let arrB = [...arrA, 4, 5, 6];
+
+// console.log(arrB);
+
+// let objA = {
+//     a: 1, 
+//     b: 2
+// };
+
+// let objB = {
+//     ...objA,
+//     c: 3,
+//     d: 4
+// };
+
+// console.log(objB);
+
+// function func(a, b, c) {
+//     console.log(a, b, c);
+// }
+
+// let arr = [1, 2, 3];
+// func(...arr);
+
+// function func(...rest) {
+//     console.log(rest);
+// }
+
+// func(1, 2, 3, 4);
+
+// function func(param, ...rest) {
+//     console.log(param);
+//     console.log(rest);
+// }
+
+// func(1, 2, 3, 4);
+
+// function func(...rest, param) {
+//     console.log(param);
+//     console.log(rest);
+// }
+
+// func(1, 2, 3, 4);
