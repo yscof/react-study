d32336a727a5452fcf303be56f94834463471e69
yscof <dexter1541@naver.com>
2026-04-28 15:29:42 +0900

    p.81

---
+++ chapter2/1.js
@@ -0,0 +1,53 @@
+// if (!undefined) {
+//     console.log("undefined는 falsy한 값입니다.");
+// }
+// if (!null) {
+//     console.log("null는 falsy한 값입니다.");
+// }
+// if (!0n) {
+//     console.log("0n는 falsy한 값입니다.");
+// }
+// if (!0) {
+//     console.log("0는 falsy한 값입니다.");
+// }
+// if (!-0) {
+//     console.log("-0는 falsy한 값입니다.");
+// }
+// if (!NaN) {
+//     console.log("NaN는 falsy한 값입니다.");
+// }
+// if (!"") {
+//     console.log('""는 falsy한 값입니다.');
+// }
+
+// const num = "false";
+
+// if (num) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// let varA;
+
+// if (varA) {
+//     console.log("값이 있음");
+// } else {
+//     console.log("값이 없음");
+// }
+
+// const num = -0;
+
+// if (num) {
+//     console.log("양수이거나 음수입니다.");
+// } else {
+//     console.log("0이거나 -0이거나 NaN입니다.");
+// }
+
+const str = "";
+
+if (str) {
+    console.log("공백 아님");
+} else {
+    console.log("공백임");
+}
