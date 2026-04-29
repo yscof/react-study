f2620d7286512a3e7a4fc48091fbefb3bb64cead
yscof <dexter1541@naver.com>
2026-04-29 20:15:09 +0900

    p.117

--- chapter2/7.js
+++ chapter2/7.js
@@ -140,12 +140,77 @@
 
 // console.log(index);
 
-let arr = [
-    { name: "이종원" }, 
-    { name: "이정환" }, 
-    { name: "신다민" }, 
-    { name: "김효빈" }, 
-];
+// let arr = [
+//     { name: "이종원" }, 
+//     { name: "이정환" }, 
+//     { name: "신다민" }, 
+//     { name: "김효빈" }, 
+// ];
 
-let element = arr.find((item) => item.name === "이정환");
-console.log(element);
+// let element = arr.find((item) => item.name === "이정환");
+// console.log(element);
+
+// let arr = [
+//     { name: "이종원", hobby: "축구" }, 
+//     { name: "이정환", hobby: "영화" }, 
+//     { name: "신다민", hobby: "축구" }, 
+//     { name: "김효빈", hobby: "노래" }, 
+// ];
+
+// let filteredArr = arr.filter(
+//     (item) => item.hobby === "축구"
+// );
+
+// console.log(filteredArr);
+
+// let arr = [1, 2, 3, 4];
+// let newArr = arr.map((item) => item * 3);
+
+// console.log(newArr);
+
+// let arr = [
+//     { name: "이종원", hobby: "축구" }, 
+//     { name: "이정환", hobby: "영화" }, 
+//     { name: "신다민", hobby: "축구" }, 
+//     { name: "김효빈", hobby: "노래" }, 
+// ];
+
+// let newArr = arr.map((item) => item.name);
+
+// console.log(newArr);
+
+// let arr = ["b", "a", "c"];
+
+// arr.sort();
+
+// console.log(arr);
+
+// let arr = [10, 5, 3];
+// arr.sort();
+
+// console.log(arr);
+
+// function compare(a, b) {
+//     if (a > b) {
+//         return 1;
+//     } else if (a < b) {
+//         return -1;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// let arr = [10, 5, 3];
+// arr.sort(compare);
+
+// console.log(arr);
+
+// let arr = ["안녕", "나는", "이정환"];
+
+// console.log(arr.join());
+// console.log(arr.join("-"));
+
+let arr = [1, 2, 3, 4, 5];
+let result = arr.reduce((acc, item) => acc + item, 0);
+
+console.log(result);
