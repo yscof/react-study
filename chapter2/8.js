38dbc2f0f7d65778afca8c6b61d9270d09061ea9
yscof <dexter1541@naver.com>
2026-04-30 15:51:45 +0900

    p.123

---
+++ chapter2/8.js
@@ -0,0 +1,62 @@
+// let date = new Date();
+
+// console.log(date);
+
+// let Jan01_1970 = new Date(0);
+
+// console.log(Jan01_1970);
+
+// let Jan02_1970 = new Date(24 * 3600 * 1000);
+
+// console.log(Jan02_1970);
+
+// let Jan02_1970 = new Date(24 * 3600 * 1000);
+
+// console.log(Jan02_1970.getTime());
+
+// let date1 = new Date("2000-10-10/00:00:00");
+// let date2 = new Date("2000.10.10/00:00:00");
+// let date3 = new Date("2000/10/10/00:00:00");
+// let date4 = new Date("2000 10 10/00:00:00");
+
+// console.log("1", date1);
+// console.log("2", date2);
+// console.log("3", date3);
+// console.log("4", date4);
+
+// let date1 = new Date(2000, 10, 10, 0, 0, 0, 0);
+// let date2 = new Date(2000, 9, 10);
+
+// console.log("1:", date1);
+// console.log("2:", date2);
+
+// let date = new Date(2000, 9, 10);
+// let timeStamp = date.getTime();
+// console.log(timeStamp);
+
+// let dateClone = new Date(timeStamp);
+// console.log(dateClone);
+
+// let date = new Date(2000, 9, 10);
+
+// console.log(date.getFullYear());
+
+// let date = new Date(2000, 9, 10);
+
+// console.log(date.getMonth());
+
+// let date = new Date(2000, 9, 10);
+
+// console.log(date.getDate());
+
+// let date = new Date(2000, 9, 10);
+
+// console.log(date.getDay());
+
+// let date = new Date(2000, 9, 10);
+
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+
