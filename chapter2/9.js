a0e3bcea6aa97353ce6fff09c4466fff65ca8971
yscof <dexter1541@naver.com>
2026-04-30 16:57:11 +0900

    p.136

--- chapter2/9.js
+++ chapter2/9.js
@@ -54,15 +54,71 @@
 // const res = double(10);
 // console.log(res);
 
-function double(num, cb) {
-    setTimeout(() => {
-        const doubleNum = num * 2;
-        cb(doubleNum);
-    }, 1000);
-}
+// function double(num, cb) {
+//     setTimeout(() => {
+//         const doubleNum = num * 2;
+//         cb(doubleNum);
+//     }, 1000);
+// }
 
-double(10, (res) => {
+// double(10, (res) => {
+//     console.log(res);
+// });
+
+// console.log("마지막");
+
+// const promise = new Promise(
+//     function(resolve, reject) {
+//         setTimeout(() => {
+//             console.log("안녕");
+//         }, 500);
+//     }
+// );
+
+// const promise = new Promise(
+//     function(resolve, reject) {
+//         setTimeout(() => {
+//             resolve("성공");
+//         }, 500);
+//     }
+// );
+
+// const promise = new Promise(
+//     function(resolve, reject) {
+//         setTimeout(() => {
+//             resolve("성공");
+//         }, 500);
+//     }
+// );
+
+// promise.then(function(res) {
+//     console.log(res);
+// });
+
+// const promise = new Promise(
+//     function(resolve, reject) {
+//         setTimeout(() => {
+//             reject("실패");
+//         }, 500);
+//     }
+// );
+
+// promise.then(function(res) {
+//     console.log(res);
+// });
+
+const promise = new Promise(
+    function(resolve, reject) {
+        setTimeout(() => {
+            reject("실패");
+        }, 500);
+    }
+);
+
+promise.then(function(res) {
     console.log(res);
 });
 
-console.log("마지막");
+promise.catch(function(err) {
+    console.log(err);
+});
