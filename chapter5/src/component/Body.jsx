c22dad6fa37ca3c22ce800e39bb142bd0e26c029
yscof <dexter1541@naver.com>
2026-05-02 18:55:04 +0900

    p.245

--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -1,49 +1,25 @@
 import { useState } from "react";
 
+function Viewer() {
+    console.log("viewer component update!");
+    return <div>Viewer</div>;
+}
+
 function Body() {
-    const [state, setState] = useState({
-        name: "",
-        gender: "", 
-        birth: "", 
-        bio: "", 
-    });
-
-    const handleOnChange = (e) => {
-        console.log("현재 수정 대상:", e.target.name);
-        console.log("수정값:", e.target.value)
-        setState({
-            ...state,
-            [e.target.name]: e.target.value,
-        });
+    const [number, setNumber] = useState(0);
+    const onIncrease = () => {
+        setNumber(number + 1);
     };
-
+    const onDecrease = () => {
+        setNumber(number - 1);
+    };
     return (
         <div>
+            <h2>{number}</h2>
+            <Viewer />
             <div>
-                <input 
-                    name="name"
-                    value={state.name} 
-                    onChange={handleOnChange} 
-                    placeholder="이름" 
-                />
-            </div>
-            <div>
-                <select name="gender" value={state.gender} onChange={handleOnChange}>
-                    <option key={""}></option>
-                    <option key={"남성"}>남성</option>
-                    <option key={"여성"}>여성</option>
-                </select>
-            </div>
-            <div>
-                <input 
-                    name="birth"
-                    type="date" 
-                    value={state.birth} 
-                    onChange={handleOnChange} 
-                />
-            </div>
-            <div>
-                <textarea name="bio" value={state.bio} onChange={handleOnChange} />
+                <button onClick={onDecrease}>-</button>
+                <button onClick={onIncrease}>+</button>
             </div>
         </div>
     );
