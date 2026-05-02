5d0045324b19c2b211e0c0dcdcaf9c43c5bd2b1e
yscof <dexter1541@naver.com>
2026-05-02 19:15:57 +0900

    p.251

--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-
-function Viewer() {
-    console.log("viewer component update!");
-    return <div>Viewer</div>;
-}
+import { useRef, useState } from "react";
 
 function Body() {
-    const [number, setNumber] = useState(0);
-    const onIncrease = () => {
-        setNumber(number + 1);
+    const [text, setText] = useState("");
+    const textRef = useRef();
+
+    const handleOnChange = (e) => {
+        setText(e.target.value);
     };
-    const onDecrease = () => {
-        setNumber(number - 1);
+
+    const handleOnClick = () => {
+        if (text.length < 5) {
+            textRef.current.focus();
+        } else {
+            alert(text);
+            setText("");
+        }
     };
+
     return (
         <div>
-            <h2>{number}</h2>
-            <Viewer />
-            <div>
-                <button onClick={onDecrease}>-</button>
-                <button onClick={onIncrease}>+</button>
-            </div>
+            <input ref={textRef} value={text} onChange={handleOnChange} />
+            <button onClick={handleOnClick}>작성 완료</button>
         </div>
     );
 }
