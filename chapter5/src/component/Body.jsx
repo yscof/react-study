48f3a9f5c266537d32f5e4f935874bb2475f2991
yscof <dexter1541@naver.com>
2026-05-02 17:27:50 +0900

    p.242

--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -1,6 +1,52 @@
-function Body({ children }) {
-    console.log(children);
-    return <div className="body">{children}</div>;
+import { useState } from "react";
+
+function Body() {
+    const [state, setState] = useState({
+        name: "",
+        gender: "", 
+        birth: "", 
+        bio: "", 
+    });
+
+    const handleOnChange = (e) => {
+        console.log("현재 수정 대상:", e.target.name);
+        console.log("수정값:", e.target.value)
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    return (
+        <div>
+            <div>
+                <input 
+                    name="name"
+                    value={state.name} 
+                    onChange={handleOnChange} 
+                    placeholder="이름" 
+                />
+            </div>
+            <div>
+                <select name="gender" value={state.gender} onChange={handleOnChange}>
+                    <option key={""}></option>
+                    <option key={"남성"}>남성</option>
+                    <option key={"여성"}>여성</option>
+                </select>
+            </div>
+            <div>
+                <input 
+                    name="birth"
+                    type="date" 
+                    value={state.birth} 
+                    onChange={handleOnChange} 
+                />
+            </div>
+            <div>
+                <textarea name="bio" value={state.bio} onChange={handleOnChange} />
+            </div>
+        </div>
+    );
 }
 
 export default Body;
