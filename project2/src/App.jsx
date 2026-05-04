d3e8e199c162fd892efe9b56079259d2d3bbec65
yscof <dexter1541@naver.com>
2026-05-04 21:25:28 +0900

    p.315

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,7 +1,16 @@
-import './App.css'
+import './App.css';
+import Header from "./component/Header.jsx";
+import TodoEditor from "./component/TodoEditor.jsx";
+import TodoList from "./component/TodoList.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor />
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
