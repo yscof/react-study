71215cbe7e31b7dd20e25e522aa4114124c3cbb2
Yongsu Ahn <dexter1541@naver.com>
2026-05-05 21:09:56 +0900

    p.320

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -4,8 +4,29 @@ import Header from "./component/Header.jsx";
 import TodoEditor from "./component/TodoEditor.jsx";
 import TodoList from "./component/TodoList.jsx";
 
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
-  const [todo, setTodo] = useState([]);
+  const [todo, setTodo] = useState(mockTodo);
   return (
     <div className="App">
       <Header />
