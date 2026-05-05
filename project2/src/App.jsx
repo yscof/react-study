e640253358487cf2aabb3d619a933f3c3877cfe7
Yongsu Ahn <dexter1541@naver.com>
2026-05-05 21:02:41 +0900

    p.318

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import './App.css';
 import Header from "./component/Header.jsx";
 import TodoEditor from "./component/TodoEditor.jsx";
 import TodoList from "./component/TodoList.jsx";
 
 function App() {
+  const [todo, setTodo] = useState([]);
   return (
     <div className="App">
       <Header />
