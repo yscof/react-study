384f9f5581865ff6369ece3862a97c1699b4c9f0
yscof <dexter1541@naver.com>
2026-05-07 19:11:32 +0900

    p.332

--- project2/src/component/TodoEditor.jsx
+++ project2/src/component/TodoEditor.jsx
@@ -15,18 +15,26 @@ const TodoEditor = ({ onCreate }) => {
         onCreate(content);
         setContent("");
     };
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onSubmit();
+        }
+    };
 
     return (
         <div className="TodoEditor">
             <h4>새로운 Todo 작성하기  ✏️</h4>
             <div className="editor_wrapper">
                 <input 
-                ref={inputRef}
-                value={content}
-                onChange={onChangeContent}
-                placeholder="새로운 Todo..." 
+                    ref={inputRef}
+                    value={content}
+                    onChange={onChangeContent}
+                    onKeyDown={onKeyDown}
+                    placeholder="새로운 Todo..." 
                 />
-                <button onClick={onSubmit}>추가</button>
+                <button onClick={onSubmit}>
+                    추가
+                </button>
             </div>
         </div>
     );
