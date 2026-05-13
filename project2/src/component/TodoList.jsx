200fce35aaed75e55ffd3e7bd5e1c7e34c2bae3a
yscof <dexter1541@naver.com>
2026-05-13 10:32:01 +0900

    검색어에 따라 필터링하기 (p.338)

--- project2/src/component/TodoList.jsx
+++ project2/src/component/TodoList.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ todo }) => {
+    const [search, setSearch] = useState("");
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+    const getSearchResult = () => {
+        return search === ""
+            ? todo 
+            : todo.filter((it) => it.content.includes(search));
+    };
+
     return (
         <div className="TodoList">
             <h4>Todo List 🌱</h4>
-            <input className="searchbar" placeholder="검색어를 입력하세요" />
+            <input
+                value={search} 
+                onChange={onChangeSearch}
+                className="searchbar" 
+                placeholder="검색어를 입력하세요"
+            />
             <div className="list_wrapper">
-                {todo.map((it) => (
+                {getSearchResult().map((it) => (
                     <TodoItem key={it.id} {...it} />
                 ))}
             </div>
