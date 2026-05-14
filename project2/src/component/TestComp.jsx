4fcf3a62de59c29a2994a44c4f26509b41ad866e
yscof <dexter1541@naver.com>
2026-05-14 21:47:40 +0900

    useReducer 이해하기 - useReducer의 기본 사용법 (p.354)

--- project2/src/component/TestComp.jsx
+++ project2/src/component/TestComp.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+function reducer() {}
 
 function TestComp() {
-    const [count, setCount] = useState(0);
-
-    const onIncrease = () => {
-        setCount(count + 1);
-    };
-
-    const onDecrease = () => {
-        setCount(count - 1);
-    };
+const [count, dispatch] = useReducer(reducer, 0);
 
     return (
         <div>
@@ -18,8 +12,12 @@ function TestComp() {
                 <strong>{count}</strong>
             </div>
             <div>
-                <button onClick={onIncrease}>+</button>
-                <button onClick={onDecrease}>-</button>
+                <button onClick={() => dispatch({ type: "INCREASE", data: 1})}>
+                    +
+                </button>
+                <button onClick={() => dispatch({ type: "DECREASE", data: 1 })}>
+                    -
+                </button>
             </div>
         </div>
     );
