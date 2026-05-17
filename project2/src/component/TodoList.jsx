baca4f348b72b0ee4fab690a3eeabf21608c0b59
yscof <dexter1541@naver.com>
2026-05-17 21:31:07 +0900

    함수의 불필요한 재호출 방지하기 - 할 일 분석 기능 추가하기 (p.369)

--- project2/src/component/TodoList.jsx
+++ project2/src/component/TodoList.jsx
@@ -14,10 +14,26 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
                 it.content.toLowerCase().includes(search.toLowerCase())
             );
     };
+    const analyzeTodo = () => {
+        const totalCount = todo.length;
+        const doneCount = todo.filter((it) => it.isDone).length;
+        const notDoneCount = totalCount - doneCount;
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount, 
+        };
+    };
+    const { totalCount, doneCount, notDoneCount } = analyzeTodo();
 
     return (
         <div className="TodoList">
             <h4>Todo List 🌱</h4>
+            <div>
+                <div>총개수: {totalCount}</div>
+                <div>완료된 할 일: {doneCount}</div>
+                <div>아직 완료하지 못한 할 일: {notDoneCount}</div>
+            </div>
             <input
                 value={search} 
                 onChange={onChangeSearch}
